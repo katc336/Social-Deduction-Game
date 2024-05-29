97ecfec15dbb9a85d926677bf8047ee44d2fe1be
Kat Christensen <katchristensen336@gmail.com>
2024-05-29 15:32:29 -0400

    Add select role tokens

--- src/client/components/GamePlay/PickRoles/RolePicker.tsx
+++ src/client/components/GamePlay/PickRoles/RolePicker.tsx
@@ -1,17 +1,18 @@
-import Alert from '@mui/material/Alert';
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { useParams } from "react-router-dom"
+import { useState } from 'react';
 import { useGetSingleGameQuery } from '../../../../redux/api';
 import MobileTheme from '../../MobileTheme';
-import scroll from "../../images/scroll.png"
-import scrollMobile from "../../images/scrollMobile.png"
-
+import scroll from "../../../images/scroll.png"
+import ReturnDashButton from '../Shared/ReturnDashButton';
 
 const RolePicker: React.FC = () => {
     const { gameId } = useParams();
     const id = Number(gameId);
-
+    const [selectedRole, setSelectedRole] = useState(-1);
     const isMobile = MobileTheme();
 
     const { data, error, isLoading } = useGetSingleGameQuery(id);
@@ -24,19 +25,51 @@ const RolePicker: React.FC = () => {
     if (data) {
         console.log(data);
     }
+
+    const handleCardClick = (role: number) => {
+        setSelectedRole(role);
+    }
+
     return (
         <div>
-                {data && data.roles.map((role: any) => (
-                    <Typography
-                        key={role.roleId}
-                        sx={{
-                            fontFamily: "fantasy",
-                            textAlign: "center",
-                            my: 1
-                        }}>
-                        {role.name}
-                    </Typography>
-                ))}
+            <ReturnDashButton />
+            <Box sx={{
+                backgroundImage: `url(${scroll})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                my: 10,
+            }}>
+                <Typography
+                    variant="h3"
+                    sx={{ my: 3, fontFamily: "fantasy", textAlign: "center" }}>
+                    Select Your Character:
+                </Typography>
+                <Stack spacing={2}>
+                    {data && data.roles.map((role: any) => (
+                        <div onClick={() => handleCardClick(role)}>
+                            <Card
+                                elevation={selectedRole === role ? 10 : 1}
+                                sx={{ width: 150, height: 150, borderRadius: 100 }}
+                            >
+                                {selectedRole === role && (
+                                    <Typography
+                                        key={role.roleId}
+                                        sx={{
+                                            fontFamily: "fantasy",
+                                            textAlign: "center",
+                                            my: 8
+                                        }}>
+                                        {role.name}
+                                    </Typography>
+                                )}
+                            </Card>
+                        </div>
+                    ))}
+                </Stack>
+                <Box>
+                </Box>
+            </Box>
         </div>
     )
 }
